83c1487a849e3b79e6905f144c97d78a5a3b4be4
Ryan Walker <ryan.walker@tombola.com>
2019-09-12 16:25:00 +0100

    Added eggs
    big ol easter ones

--- Rockets.js
+++ Rockets.js
@@ -62,3 +62,181 @@ function changeText () {
     document.getElementById("rocket_mars").innerHTML = rocketMars[selected];
     document.getElementById("rocket_image").src = rocketImage[selected];
 }
+
+
+
+
+
+
+
+
+// JavaSnake
+var circle = ["_", "\\", "|", "/" ];
+var statusPos = 0;
+
+var gridSize = 20;
+var grid;
+
+var appleX = 3;
+var appleY = 3;
+
+var playerX;
+var playerY;
+var playerLength;
+var score;
+var playerDir;
+var lastPlayerDir;
+var playerPreviousX;
+var playerPreviousY;
+var dead = true;
+var cooldowned = true;
+
+function setVars (){
+playerX = 8;
+playerY = 8;
+playerLength = 3;
+score = 0;
+playerDir = 'right';
+playerPreviousX = [];
+playerPreviousY = [];
+cooldowned = true;
+}
+
+console.clear();
+cooldown();
+
+function tick() {
+  console.clear();
+  console.log('JavaSnake V1.0');
+  
+  statusBar();
+  update();
+  displayGrid();
+  
+  if (!dead){
+      setTimeout(tick, 120);
+  }
+}
+
+document.onkeydown = function(e) {
+  if (!dead){
+      switch (e.which) {
+    case 87:if (lastPlayerDir != 'down') playerDir = 'up'; break;
+    case 68:if (lastPlayerDir != 'left') playerDir = 'right'; break;
+    case 83:if (lastPlayerDir != 'up') playerDir = 'down'; break;
+    case 65:if (lastPlayerDir != 'right') playerDir = 'left'; break;
+      }
+  } else if (cooldowned) {
+    respawn();
+  }
+}
+
+function update() {
+  lastPlayerDir = playerDir;
+  grid = new Array(gridSize);
+  for (var y = 0; y < gridSize; y++) {
+    grid[y] = new Array(gridSize);
+    for (var x = 0; x < gridSize; x++){
+      grid[y][x] = '_';
+    }
+  } 
+  movePlayer(playerDir);
+}
+
+function movePlayer(direction){
+  grid[playerY][playerX] = '0';
+  
+  for (var i = 0; i < playerLength - 1; i++) {
+    if (playerPreviousX[i] == null){
+      if (playerPreviousY[playerPreviousY.length - 1] == null){
+        playerPreviousY[i] = playerY;
+        playerPreviousX[i] = playerX;
+      } else {
+         playerPreviousY[i] = playerPreviousY[playerPreviousY.length - 1];
+        playerPreviousX[i] = playerPreviousX[playerPreviousX.length - 1];
+      }
+    }
+    grid[playerPreviousY[i]][playerPreviousX[i]] = '0';
+  }
+  playerPreviousX.unshift(playerX);
+  playerPreviousY.unshift(playerY);
+  if (playerPreviousX.length >= playerLength){
+    playerPreviousX.pop();
+    playerPreviousY.pop();
+  }
+  
+  grid[appleY][appleX] = '@';
+  
+    switch (direction) {
+    case 'up': playerY--; break;
+    case 'right': playerX++; break;
+    case 'down': playerY++; break;
+    case 'left': playerX--; break;
+  }
+  
+  playerX = clamp(playerX, 0, gridSize - 1);
+  playerY = clamp(playerY, 0, gridSize - 1);
+  
+
+  if (grid[playerY][playerX] == '@'){
+    generateApple();
+    score++;
+    playerLength += 2;
+  } else if (grid[playerY][playerX] === '0'){
+    gameOver();
+  }
+}
+
+function generateApple () {
+  do {
+  appleX = Math.floor(Math.random() * gridSize);
+  appleY = Math.floor(Math.random() * gridSize);
+  } while (grid[appleY][appleX] != '_')
+}
+
+function statusBar(){
+  console.log("Running... " + circle[statusPos]);
+  if (statusPos > circle.length - 2) {
+    statusPos = 0;
+  } else {
+      statusPos++;
+  }
+}
+
+
+function displayGrid () {
+  var gridText = '\n';
+  for (var y = 0; y < gridSize; y++) {
+    for (var x = 0; x < gridSize; x++){
+      gridText += '|';
+      gridText += grid[y][x];
+      if (x === gridSize - 1) {
+        gridText += '|';
+      }
+    }
+    gridText += '\n';  
+  }
+  console.log(gridText);
+}
+
+function clamp(num, min, max) {
+  return num <= min ? min : num >= max ? max : num;
+}
+  
+function gameOver(){
+  dead = true;
+  cooldowned = false;
+  setTimeout(cooldown, 1000);
+  console.log('You Died! \n Score: ' + score + '\n')
+}
+
+function respawn(){
+  dead = false;
+  tick();
+  generateApple();
+}
+  
+function cooldown(){
+  setVars();
+  console.log('Press any key to continue.');
+}
